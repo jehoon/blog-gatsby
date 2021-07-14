93c61a37fde6dbbae10762bc1ac17c7c88d307db
jehoon.park <irubeta01@gmail.com>
2021-07-15 02:07:46 +0900

    chore: 블로그 테마 셋팅
    - 하 v3 튜토리얼 뭔데 ㅠㅠ

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "blog jehoon",
+    siteUrl: 'https://jehoon.gatsbyjs.io',
+    title: 'blog jehoon',
+    author: `My Name`,
+    description: '글 꾸준히 써보려고 만듬',
+    // Used for resolving images in social cards
+    siteUrl: `https://jehoon.gatsbyjs.io`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Github`,
+        url: `https://github.com/jehoon`,
+      },
+    ],
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud", {
+    resolve: `gatsby-theme-blog`,
+    options: {
+      // basePath defaults to `/`
+      basePath: `/blog`,
+    },
+  },],
 };
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+import { Themed } from 'theme-ui'
+
+/**
+ * Shadow me to add your own bio content
+ */
+const BioContent = () => (
+  <Fragment>
+    Words by <Themed.a href="http://github.com/jehoon">jehoon.park</Themed.a>.
+    <br/>
+    하하하
+  </Fragment>
+)
+
+export default BioContent;
